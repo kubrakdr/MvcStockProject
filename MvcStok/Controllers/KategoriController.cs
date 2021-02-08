95d12791d60d8cb04f4c3ee89dfb81575dea3dbe
Kubra <kubrakidir@gmail.com>
2021-02-08 19:48:23 +0300

    Add project files.

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MvcStok.Models.Entity;
+using System.Web.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DbMvcStokEntities1 db = new DbMvcStokEntities1(); 
+        public ActionResult Index()
+        {
+            var kategoriler = db.tblKategori.ToList();
+            return View(kategoriler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult YeniKategori(tblKategori p)
+        {
+            db.tblKategori.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var ktg = db.tblKategori.Find(id);
+            db.tblKategori.Remove(ktg);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.tblKategori.Find(id);
+            return View("KategoriGetir", ktgr);
+
+        }
+
+
+    }
+}
