a71c36c71e653df3064e89d726224f8dc934bd8b
Kubra <kubrakidir@gmail.com>
2021-02-18 23:54:29 +0300

    satislar listeleme

---
+++ MvcStok/Controllers/SatisController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        DbMvcStokEntities1 db = new DbMvcStokEntities1();
+
+        public ActionResult Index()
+        {
+            var satislar = db.tblSatis.ToList();
+            return View(satislar);
+        }
+    }
+}
